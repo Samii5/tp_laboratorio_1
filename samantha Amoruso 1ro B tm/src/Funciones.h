44eb84381d29d8a44dcecc297a69e8d96bb68b97
Samii5 <samiymelu111@gmail.com>
2021-04-14 19:47:25 -0300

    termina3

--- samantha Amoruso 1ro B tm/src/Funciones.h
+++ samantha Amoruso 1ro B tm/src/Funciones.h
@@ -1,8 +1,49 @@
 #include <stdio.h>
-
+/**
+ * @fn float sumar(float, float)
+ * @brief suma 2 numeros
+ *
+ * @param el primer numero a ser sumado: NumeroUno
+ * @param el segundo numero a ser sumado: NumeroDos
+ * @return la suma
+ */
 float sumar(float NumeroUno, float NumeroDos);
+
+/**
+ * @fn float restar(float, float)
+ * @brief resta 2 numeros
+ *
+ * @param el primer numero a ser restado: NumeroUno
+ * @param el segundo numero a ser restado: NumeroDos
+ * @return la resta
+ */
 float restar(float NumeroUno, float NumeroDos);
+
+/**
+ * @fn float multiplicar(float, float)
+ * @brief
+ *
+ * @param el primer numero que multiplica : NumeroUno
+ * @param el segundo numero que multiplica: NumeroDos
+ * @return la multiplicacion
+ */
 float multiplicar(float NumeroUno, float NumeroDos);
+
+/**
+ * @fn float dividir(float, float)
+ * @brief
+ *
+ * @param el primer numero que divide: NumeroUno
+ * @param el segundo numero que divide: NumeroDos
+ * @return la division
+ */
 float dividir(float NumeroUno, float NumeroDos);
+
+/**
+ * @fn float factorial(float)
+ * @brief
+ *
+ * @param el factorial de un numero: NumeroUno
+ * @return el factorial
+ */
 float factorial(float NumeroUno);
-float factorial(float NumeroDos);
